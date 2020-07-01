f131531ebf119b688bbc4c496b0409dc29b97991
Maksim Koshman <maksimkoshman090@gmail.com>
2020-07-01 09:18:30 +0300

    create registration page

--- js/_custom.js
+++ js/_custom.js
@@ -1,14 +1,18 @@
 $(document).ready(function () {
+  // ЛИЧНЫЙ КАБИНЕТ
+
   // Burger menu
   $(".lk-burger").on("click", function () {
     $(this).toggleClass("active");
     $("#lkMenu").toggleClass("active");
+    $("#personalMenu").removeClass("active");
     $("body").toggleClass("overflow");
   });
 
   $(".personal-burger").on("click", function () {
     $(this).toggleClass("active");
     $("#personalMenu").toggleClass("active");
+    $("#lkMenu").removeClass("active");
     $("body").toggleClass("overflow");
     $("section").toggleClass("overflow");
   });
@@ -18,7 +22,7 @@ $(document).ready(function () {
   $("[data-scroll]").on("click", function (event) {
     event.preventDefault();
 
-    var $this = $(this),
+    let $this = $(this),
       blockId = $this.data("scroll"),
       blockOffset = $(blockId).offset().top;
 
@@ -36,93 +40,44 @@ $(document).ready(function () {
       500
     );
   });
+  // ЛИЧНЫЙ КАБИНЕТ END
 
-  // Show more -- Agency
-  $(".agency__link").on("click", function (event) {
-    event.preventDefault();
-    $(this).hide();
-    $(".agency__text").removeClass("hidden");
+  // РЕГИСТРАЦИЯ
+
+  // Выпадающий список
+  $(".registration-form-dropdown").on("click", function () {
+    $(this).toggleClass("show");
   });
 
-  // Intro Map
-  $(".intro__map__item").on("click", function () {
-    $(".intro__map__item").removeClass("active");
+  $(".registration-form-dropdown-list-value").on("click", function () {
+    $(".registration-form-dropdown-text").text($(this).text());
+  });
+
+  // Навигация
+  $("[data-content=registrationContent-1]").on("click", function () {
+    $(".registration-item").removeClass("active");
+    $(".registration-content").removeClass("active");
     $(this).addClass("active");
-
-    if ($(this).data("slide") == "1") {
-      galleryTop.slideTo(1, 1000);
-    } else if ($(this).data("slide") == "2") {
-      galleryTop.slideTo(2, 1000);
-    } else if ($(this).data("slide") == "3") {
-      galleryTop.slideTo(3, 1000);
-    } else if ($(this).data("slide") == "4") {
-      galleryTop.slideTo(4, 1000);
-    }
+    $("#registrationContent-1").addClass("active");
   });
 
-  // Modal Window
-  $(".modal__close").on("click", function () {
-    $(".modal__mask").hide(500);
+  $("[data-content=registrationContent-2]").on("click", function () {
+    $(".registration-item").removeClass("active");
+    $(".registration-content").removeClass("active");
+    $(this).addClass("active");
+    $("#registrationContent-2").addClass("active");
   });
 
-  $(document).keyup(function (e) {
-    if (e.key === "Escape") {
-      $(".modal__mask").hide(500);
-    }
+  $("[data-content=registrationContent-3]").on("click", function () {
+    $(".registration-item").removeClass("active");
+    $(".registration-content").removeClass("active");
+    $(this).addClass("active");
+    $("#registrationContent-3").addClass("active");
   });
 
-  $(document).mouseup(function (e) {
-    let modalctr = $(".modal__mask");
-    let modal = $(".modal__window");
-    if (!modal.is(e.target) && modal.has(e.target).length === 0) {
-      modalctr.hide();
-    }
-  });
-
-  $(".projects__link").on("click", function (event) {
-    event.preventDefault();
-    $(".modal__mask").show(500);
-  });
-
-  // Detail New
-  var newsdetailhumbs = new Swiper(".newsdetail-thumbs", {
-    spaceBetween: 4,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
-  });
-  var newsdetailTop = new Swiper(".newsdetail-top", {
-    spaceBetween: 4,
-    navigation: {
-      nextEl: ".newsdetail__next",
-      prevEl: ".newsdetail__prev",
-    },
-    thumbs: {
-      swiper: newsdetailhumbs,
-    },
-    pagination: {
-      el: ".newsdetail__progress",
-      type: "progressbar",
-    },
-    breakpoints: {
-      1199: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 1,
-      },
-      320: {
-        slidesPerView: 1.1,
-      },
-    },
-  });
-
-  // News List
-  $(".newslist__shormore").on("click", function (event) {
-    event.preventDefault();
-    $(this).hide();
-    $(".newslist__item--special").removeClass("hidden");
-    $(".newslist__nav--tablet .intro__nav__item").removeClass("hidden");
-  });
+  // темные крошки на мобилках
+  if ($(window).width() < 992) {
+    $("#locationDocument").addClass("location--dark");
+  }
+  // РЕГИСТАЦИЯ END
 });
